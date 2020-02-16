34d7898c86f9c014c938442dbb933a850e4a0daf
Jelani Woods <jelani@firstdraft.com>
2020-02-16 17:09:30 -0600

    Add password digest & update appcontrler

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,18 @@
 #
 # Table name: users
 #
-#  id             :integer          not null, primary key
-#  comments_count :integer
-#  likes_count    :integer
-#  private        :boolean
-#  username       :string
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
+#  id              :integer          not null, primary key
+#  comments_count  :integer
+#  likes_count     :integer
+#  password_digest :string
+#  private         :boolean
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 
 class User < ApplicationRecord
-
+  has_secure_password
   validates(:username, { :presence => true })
 
   def comments
